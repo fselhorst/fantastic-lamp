cf4bb724f7594f025ec2dd6de74724c58f8c048d
Frank Selhorst <frankselhorst@Franks-MacBook-Pro.local>
2020-11-28 00:26:02 +0100

    add cors

--- api/index.js
+++ api/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
+
+app.use(cors());
 
 const PORT = process.env.PORT || 8080;
 const API_CONTEXT = process.env.API_CONTEXT || '/api'
